869bf9f93edf642df2b9a51f5f076cb649c41a7b
ananta khodake <ananta.khodake02@gmail.com>
2024-03-13 23:36:59 +0530

    update ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { AiFillDelete  } from "react-icons/ai";
+import { BsCheckLg } from "react-icons/bs";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isCompleateScreen, setIsCompleateScreen] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo app</h1>
+
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title:</label>
+            <input type="text" placeholder="Enter the title?"></input>
+          </div>
+          <div className="todo-input-item">
+            <label>Description:</label>
+            <input
+              type="text"
+              placeholder="Enter the title description"
+            ></input>
+          </div>
+          <div className="todo-input-item">
+            <button type="button" className="primary-btn">
+              Add
+            </button>
+          </div>
+        </div>
+        <div className="button-area">
+          <button
+            className={`secondary-btn ${
+              isCompleateScreen === false && "active"
+            }`}
+            onClick={() => setIsCompleateScreen(false)}
+          >
+            Todo
+          </button>
+          <button
+            className={`secondary-btn ${
+              isCompleateScreen === true && "active"
+            }`}
+            onClick={() => setIsCompleateScreen(true)}
+          >
+            Compleated
+          </button>
+        </div>
+
+        <div className="todo-list">
+          <div className="todo-list-item">
+            <h3>Task1</h3>
+            <p>Description</p>
+          </div>
+          <div>
+            <AiFillDelete className="icon" />
+            <BsCheckLg  className="icon" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
